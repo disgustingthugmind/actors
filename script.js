a818e8463b4474a2f3f7be9bccc6ef75dae9fa76
disgustingthugmind <80728740+disgustingthugmind@users.noreply.github.com>
2022-02-09 23:23:34 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var box1 = document.querySelector('.div1')
+var box2 = document.querySelector('.div2')
+var box3 = document.querySelector('.div3')
+
+box1.onmousedown = function () {
+    box1.style.backgroundImage = 'url(kon2.png)'
+}
+box2.onmousedown = function () {
+    box2.style.backgroundImage = 'url(lim2.png)'
+}
+box3.onmousedown = function () {
+    box3.style.backgroundImage = 'url(nam2.png)'
+}
